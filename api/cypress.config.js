356a7dbb4b2870672c6fb74130dfa0d9c2440c16
hllsouza <bugatoo@gmail.com>
2025-03-14 14:03:47 -0300

    atualizando relatorio para mochawesome

--- api/cypress.config.js
+++ api/cypress.config.js
@@ -1,24 +1,33 @@
-const dotenv = require("dotenv");
 const { defineConfig } = require("cypress");
-const registerCypressGrep = require("cypress-grep/src/plugin");
+const path = require('path');
 
-dotenv.config();
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true,
+    reportPageTitle: 'Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveJson: true,
+    reportFilename: '[name]-report'
+  },
   e2e: {
     baseUrl: "https://serverest.dev",
     video: false,
-    screenshotOnRunFailure: false,
+    screenshotOnRunFailure: true,
     defaultCommandTimeout: 10000,
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       config.env.email_admin = process.env.EMAIL_ADMIN;
       config.env.password_admin = process.env.PASSWORD_ADMIN;
-
-      registerCypressGrep(config);
       return config;
-    },
-    env: {
-      grepFilterSpecs: true,
-    },
-  },
+    }
+  }
 });
